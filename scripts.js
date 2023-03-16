d6a7c998349e364264949786521b5cc7c165a7fb
sidneigoulartjunior <goulartconsultor1@gmail.com>
2023-03-16 10:03:02 -0300

    Projeto Breaking Bad

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+        
+        botao.classList.add('selecionado');
+
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        imagens[indice].classList.add('ativa');
+    })
+})
